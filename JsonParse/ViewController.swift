e9a433a70da2c583136aeef029156b11a4cc0872
A70093 <eA70093@tdcc.com.tw>
2022-04-26 11:08:56 +0800

    Initial Commit

---
+++ JsonParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonParse
+//
+//  Created by TDCC_IFD on 2022/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
